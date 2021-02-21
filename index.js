e20bab1b28b07101043d0469b74080bdc2cc3b92
Eldar <77222803+EldarSemenov@users.noreply.github.com>
2021-02-22 08:40:55 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.getElementById('title').innerHTML = "WELCOME";
+document.getElementById('first-paragraph').innerHTML = "Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wnating to share ideas, tips, and photos with other like-minded people.";
+document.getElementById('second-paragraph').innerHTML = "So don't waste time! Get what you need, then turn off that computer and get out into the great outdoors!";
+document.getElementById('third-paragraph').innerHTML = "P.S. The project: 'MDN Mozilla; Document and website structure' was made by Eldar Semenov :)"

5a9d637a60941857b4919126f898e3c78311f44b
Eldar <77222803+EldarSemenov@users.noreply.github.com>
2022-10-24 10:12:20 +1000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+// 2. Strings I
+
+//CHARACTER ACCESS
+
+const language = 'JavaScript';
+console.log(language[0]); // J
+
+//COMBINING IT WITH LENGTH
+
+console.log(language[language.length - 6]); // S
+
+//THE at.(index) METHOD
+
+console.log(language.at(0)) // J
+console.log(language.at(4)) // S 
+console.log(language.at(-6)) // S
+
+

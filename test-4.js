716ff37505f612ff62ed93b21979a14ac92d1ce4
Eldar <77222803+EldarSemenov@users.noreply.github.com>
2022-10-26 11:20:54 +1000

    Add files via upload

---
+++ test-4.js
@@ -0,0 +1,28 @@
+// 3. Numbers
+
+//    OPERATIONS
+
+// DIVISION REMAINDER
+
+console.log(8 % 2); // Division remainder is o
+
+console.log(7 % 2); // Division remainder is 1
+
+// NUMBER METHODS
+
+console.log(Math.round(2.6)); // 3
+console.log(Math.floor(2.6)); // 2
+
+// The Math.ceil() method rounds a number rounded UP to the nearest integer.
+console.log(Math.ceil(2.6)); // 3
+console.log(Math.ceil(2.4)); // 3
+console.log(Math.ceil(2.5)); // 3
+
+// other samples:
+
+console.log(Math.round(2.5)); // 3
+console.log(Math.floor(2.5)); // 2
+console.log(Math.ceil(2.5)); // 3
+
+
+

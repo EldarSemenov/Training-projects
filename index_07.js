b7ba81a3ab780cd23d1f56ee082357bb7bf0cec7
Eldar <77222803+EldarSemenov@users.noreply.github.com>
2023-07-02 09:37:34 +1000

    Add files via upload

--- index_07.js
+++ index_07.js
@@ -1,24 +1,12 @@
-const form = document.querySelector("#form-text");
-const textInput = document.querySelector(".input-text");
-const outputText = document.querySelector(".text");
-const container = document.querySelector(".text-result");
-const paragraph = document.querySelector(".text-result p");
-const clean = document.querySelector("#clean");
+const input = document.querySelector("#input-description");
+const output = document.querySelector("#output-description");
+const outputContainer = document.querySelector(".output-container");
 
 const getDescription = text => {
-    return text.substring(0, 10);
+    return text.substring(0, 10) + "...";
 };
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
-    outputText.textContent = getDescription(textInput.value);
-    container.classList.add("active");
-    paragraph.classList.add("show");
-});
-clean.addEventListener("click", () => {
-    container.classList.remove("active");
-    paragraph.classList.remove("show");
-    textInput.value = "";
-    textInput.focus();
-    outputText.textContent = "";
+input.addEventListener("input", event => {
+    output.textContent = getDescription(event.currentTarget.value);
+    outputContainer.classList.add("active");
 });

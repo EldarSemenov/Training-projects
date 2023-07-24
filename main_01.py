6b5745d65832c811145a94887918d03a472a1208
Eldar <77222803+EldarSemenov@users.noreply.github.com>
2023-07-24 13:20:52 +1000

    Add files via upload

---
+++ main_01.py
@@ -0,0 +1,21 @@
+#1.5.2 Assign Multiple Values
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#One Value to Multiple Variables
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+
+cars = ["AppleCar M", "Tesla Model 2", "BMW M3"]
+x, y, z = cars
+print(x)
+print(y)
+print(z)
